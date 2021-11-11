d2ef9f077a59805b119d5bf722af5062cc55261c
yomimono <maintenance@identity-function.com>
2021-11-10 19:20:00 -0600

    add list function

--- mirage/fs.ml
+++ mirage/fs.ml
@@ -31,22 +31,23 @@ module Make(Sectors: Mirage_block.S) = struct
     let next _ = (2l, 3l)
   end
 
+  let block_of_block_number {block_size; block; program_block_size; _} block_location =
+    let cs = Cstruct.create block_size in
+    This_Block.read block block_location [cs] >>= function
+    | Error b -> Lwt.return (Error (`Block b))
+    | Ok () ->
+      match Littlefs.Block.of_cstruct ~program_block_size cs with
+      | Error _ -> Lwt.return (Error (`Littlefs_read))
+      | Ok extant_block -> Lwt.return (Ok extant_block)
+
   let connect device ~program_block_size ~block_size : (t, error) result Lwt.t =
+    (* TODO: for now, everything we would care about
+     * from reading the FS is either hardcoded in
+     * the implementation, or needs to be provided
+     * in order to read the filesystem. For now, if we can
+     * connect to the underlying device, call that good enough. *)
     This_Block.connect ~block_size device >>= fun block ->
-    let block_0, block_1 = Cstruct.(create block_size, create block_size) in
-    This_Block.read block 0L [block_0] >>= fun _ ->
-    This_Block.read block (Int64.of_int block_size) [block_1] >>= fun _ ->
-    (* TODO: we should see which is the more recent write and treat that one as authoritative *)
-    match Littlefs.Block.of_cstruct ~program_block_size block_0 with
-    | _b1 -> begin
-        (* TODO: for now, everything we would care about
-         * from reading the FS is either hardcoded in
-         * the implementation, or needs to be provided
-         * in order to read the filesystem. If we can
-         * make some meaning out of the blocks, call
-         * that good enough. *)
-        Lwt.return (Ok {block; block_size; program_block_size})
-      end
+    Lwt.return (Ok {block; block_size; program_block_size})
 
   let format t =
     let program_block_size = t.program_block_size in
@@ -62,12 +63,12 @@ module Make(Sectors: Mirage_block.S) = struct
     write_whole_block 0L block_0 >>= fun _ ->
     write_whole_block 1L block_1
   
-  let id_of_key block key =
+  let id_of_key (ty, chunk) block key =
     let data_matches c =
       0 = Mirage_kv.Key.(compare key @@ v @@ Cstruct.to_string c)
     in
     let tag_matches t =
-      Littlefs.Tag.(fst t.type3 = LFS_TYPE_NAME && snd t.type3 = 0x01)
+      Littlefs.Tag.(fst t.type3 = ty && snd t.type3 = chunk)
     in
     List.find_map (fun c ->
         match List.find_opt (fun (tag, data) ->
@@ -77,33 +78,58 @@ module Make(Sectors: Mirage_block.S) = struct
         | None -> None
       ) (Littlefs.Block.commits block)
 
-  let find_inline_file_contents block id =
+  let entries_of_id block id =
     let commits = Littlefs.Block.commits block in
-    (* TODO: we shouldn't keep searching after one result *)
     let matches (tag, _) =
-      let type3, chunk = tag.Littlefs.Tag.type3 in
-      0 = compare tag.Littlefs.Tag.id id &&
-      type3 = Littlefs.Tag.LFS_TYPE_STRUCT &&
-      chunk = 0x01
+      0 = compare tag.Littlefs.Tag.id id
     in
-    let aux c = List.find_opt matches (Littlefs.Commit.entries c) in
-    List.find_map aux commits
+    let aux c = List.find_all matches (Littlefs.Commit.entries c) in
+    List.(flatten @@ map aux commits)
 
-  let get {program_block_size; block; block_size; } key =
-    (* TODO: use the filesystem to find the right blocks to search *)
+  (* for now, ignore key -- in the future, we'll look up which metadata
+   * pairs it points to and list those instead of block 0 *)
+  let list t _key =
     let block_location = 0L in
-    let cs = Cstruct.create block_size in
-    This_Block.read block block_location [cs] >>= function
-    | Error b -> Lwt.return (Error (`Block b))
-    | Ok () ->
-      match Littlefs.Block.of_cstruct ~program_block_size cs with
-      | Error _ -> Lwt.fail_with "couldn't read block"
+    block_of_block_number t block_location >>= function
+    | Error _ as e -> Lwt.return e
+    | Ok extant_block ->
+      (* we want to list all names in all commits in the block,
+       * preserving information about which of them are files and which directories *)
+      let info_of_entry (tag, data) =
+        match tag.Littlefs.Tag.type3 with
+        | (LFS_TYPE_NAME, 0x01) ->
+          Some (Cstruct.to_string data, `Value)
+        | (LFS_TYPE_NAME, 0x02) ->
+          Some (Cstruct.to_string data, `Dictionary)
+        | _ -> None
+      in
+      let commits = Littlefs.Block.commits extant_block in
+      let relevant_entries c = List.filter_map info_of_entry
+          (Littlefs.Commit.entries c)
+      in
+      Lwt.return @@ Ok (List.flatten @@ List.map relevant_entries commits)
+
+  let get t key =
+    let get_from_block block_location =
+      block_of_block_number t block_location >>= function
+      | Error _ as e -> Lwt.return e
       | Ok extant_block ->
-        match id_of_key extant_block key with
+        match id_of_key (Littlefs.Tag.LFS_TYPE_NAME, 0x01) extant_block key with
         | None -> Lwt.return (Error (`Not_found key))
-        | Some (_commit, id) -> match find_inline_file_contents extant_block id with
-          | None -> Lwt.return (Error (`Not_found key))
-          | Some (_tag, data) -> Lwt.return (Ok (Cstruct.to_string data))
+        | Some (_commit, id) ->
+          match entries_of_id extant_block id with
+          | [] -> Lwt.return (Error (`Not_found key))
+          | l ->
+            match List.find_opt (fun (tag, _data) ->
+                Littlefs.Tag.((fst tag.type3) = LFS_TYPE_STRUCT) &&
+                Littlefs.Tag.((snd tag.type3) = 0x01)
+              ) l with
+          (* TODO: we should make sure the dictionary entry is there before returning this error *)
+            | None -> Lwt.return (Error (`Value_expected key))
+            | Some (_tag, data) -> Lwt.return (Ok (Cstruct.to_string data))
+    in
+    (* TODO: we have a bit more work to do to discover the right block ;) *)
+    get_from_block 0L
 
   (* TODO: we really need a convenience function for "read me this pair of blocks" *)
 
--- src/lfs_ls.ml
+++ src/lfs_ls.ml
@@ -10,24 +10,33 @@ let block_size =
   Cmdliner.Arg.(value & pos 1 int 4096 & info ~doc ~docv:"BLOCK_SIZE" [])
 
 let path =
-  let doc = "path to write. Currently any missing hierarchy will not be created,
+  let doc = "path to list. Currently any missing hierarchy will not be created,
              and indeed no hierarchy is supported. In effect, this is just a filename." in
   Cmdliner.Arg.(value & pos 2 string "example" & info ~doc ~docv:"PATH" [])
 
-let read image block_size path =
+let ls image block_size path =
   let open Lwt.Infix in
   Lwt_main.run @@ (
   Mirage_block.connect image >>= fun block ->
   Littlefs.connect block ~program_block_size:16 ~block_size >>= function
-  | Error _ -> Stdlib.Format.eprintf "Error doing the initial filesystem read\n%!"; exit 1
+  | Error _ -> Stdlib.Format.eprintf "Error doing the initial filesystem ls\n%!"; exit 1
   | Ok t ->
-    Littlefs.get t (Mirage_kv.Key.v path) >>= function
-    | Ok v -> Stdlib.Format.printf "%s%!" v; Lwt.return_unit
+    Littlefs.list t (Mirage_kv.Key.v path) >>= function
     | Error (`Not_found _key) -> Stdlib.Format.eprintf "key %s not found\n%!" path; exit 1
     | Error (`Value_expected _key) -> Stdlib.Format.eprintf "%s isn't bound to a key\n%!" path; exit 1
-    | Error _ -> Stdlib.Format.eprintf "filesystem was opened, but read failed\n%!"; exit 2
+    | Error _ -> Stdlib.Format.eprintf "filesystem was opened, but ls failed\n%!"; exit 2
+    | Ok l ->
+      let pp fmt (name, key_or_dict) =
+        let pp_ty fmt = function
+          | `Value -> Stdlib.Format.fprintf fmt "%s" "file"
+          | `Dictionary -> Stdlib.Format.fprintf fmt "%s" "directory"
+        in
+        Stdlib.Format.fprintf fmt "%s : %a" name pp_ty key_or_dict
+      in
+      List.iter (fun e -> Stdlib.Format.printf "%a\n%!" pp e) l
+    ; Lwt.return_unit
 )
 
 let () =
-  let go = Cmdliner.Term.(const read $ image $ block_size $ path) in
-  Cmdliner.Term.(exit @@ eval (go, info "lfs_read"))
+  let go = Cmdliner.Term.(const ls $ image $ block_size $ path) in
+  Cmdliner.Term.(exit @@ eval (go, info "lfs_ls"))
