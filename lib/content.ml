38316439dbc4e266dbce241fb8d7579b53cc399a
yomimono <maintenance@identity-function.com>
2022-07-17 15:03:22 -0500

    skip tags with ids 0 and 0x3ff

--- lib/content.ml
+++ lib/content.ml
@@ -1,15 +1,19 @@
 let size (tag, data) =
-  match tag.Tag.type3 with
-  | Tag.LFS_TYPE_STRUCT, c when c = File.inline_struct_chunk ->
-    `File tag.Tag.length
-  | Tag.LFS_TYPE_STRUCT, c when c = File.ctz_chunk -> begin
+  if tag.Tag.id >= 0x3ff || tag.Tag.id <= 0x00
+  then `Skip
+  else begin
+    match tag.Tag.type3 with
+    | Tag.LFS_TYPE_STRUCT, c when c = Tag.Magic.struct_inline ->
+      `File tag.Tag.length
+    | Tag.LFS_TYPE_STRUCT, c when c = Tag.Magic.struct_ctz -> begin
       match File.ctz_of_cstruct data with
       | None -> `Skip
       | Some (_, file_size) -> `File (Int32.to_int file_size)
     end
-  | Tag.LFS_TYPE_STRUCT, c when c = 0x00 -> begin
-    match Dir.dirstruct_of_cstruct data with
-    | Some pair -> `Dir pair
-    | None -> `Skip
+    | Tag.LFS_TYPE_STRUCT, c when c = Tag.Magic.struct_dir -> begin
+        match Dir.dirstruct_of_cstruct data with
+        | Some pair -> `Dir pair
+        | None -> `Skip
+      end
+    | _ -> `Skip
   end
-  | _ -> `Skip
